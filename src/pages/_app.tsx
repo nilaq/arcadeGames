85c57f9f57afcce9249c7a138024807c3aa1ff2d
nilaq <nbitzer@stanford.edu>
2023-03-27 13:15:14 -0700

    deploy analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Jost } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 import { api } from "../utils/api";
 
@@ -13,17 +14,18 @@ const jost = Jost({
 });
 
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <main className={`${jost.variable} font-sans`}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
-  );
+const MyApp: AppType<{ session: Session | null }> = ({Component,pageProps: { session, ...pageProps },
+                                                     }) => {
+    return (
+        <>
+            <SessionProvider session={session}>
+                <main className={`${jost.variable} font-sans`}>
+                    <Component {...pageProps} />
+                </main>
+            </SessionProvider>
+            <Analytics/>
+        </>
+    );
 };
 
 export default api.withTRPC(MyApp);
